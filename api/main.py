986f6406f23533c63a37e3137f114acd82d09f48
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-08-05 08:55:02 +0530

    feat: set up rate limiting to protect API endpoints from excessive client requests

--- api/main.py
+++ api/main.py
@@ -41,3 +41,8 @@ async def lifespan(app: FastAPI):
 
 # Creating FastAPI App Instance
 app = FastAPI(title='AutoIQ by Motor.co', lifespan=lifespan)
+
+# Setting up Rate Limiter
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
